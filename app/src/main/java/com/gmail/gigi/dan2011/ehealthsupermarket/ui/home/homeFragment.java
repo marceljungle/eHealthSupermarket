169eca78f3ca2e223a8f3b2ea91f3aabb5174d4f
sergypl <sergiovinagorro@gmail.com>
2021-02-22 19:07:26 +0100

    add and modified items in the home page

--- app/src/main/java/com/gmail/gigi/dan2011/ehealthsupermarket/ui/home/homeFragment.java
+++ app/src/main/java/com/gmail/gigi/dan2011/ehealthsupermarket/ui/home/homeFragment.java
@@ -1,9 +1,12 @@
 package com.gmail.gigi.dan2011.ehealthsupermarket.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +16,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.gmail.gigi.dan2011.ehealthsupermarket.R;
+import com.gmail.gigi.dan2011.ehealthsupermarket.activity_productview;
 
 public class homeFragment extends Fragment {
 
@@ -21,6 +25,19 @@ public class homeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(homeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        /*  Button to view the activity that contains the product information.*/
+        ImageButton product = (ImageButton) root.findViewById(R.id.imagebuttonproduct2);
+        product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), activity_productview.class);
+                startActivity(in);
+            }
+        });
+
         return root;
     }
+
+
 }
