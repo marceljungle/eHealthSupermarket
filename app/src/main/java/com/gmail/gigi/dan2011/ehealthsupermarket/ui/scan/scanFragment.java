1cef5e5d698b820477e48e5f2107f47e1318420e
Gigi Marcel Dan <gigi.dan2011@gmail.com>
2021-02-27 18:51:56 +0100

    Implemented provisional camera action in the scan menu. Read more...
    We need to implement this functionality using Camera2 API otherwise
    we can't take photos without user interaction e.g. pressing a
    button, and if we want to make this funcionality like qr scanners, or
    google lens for example, without user interaction, we use that API.
    
    Here is a repo that offers an easy to use it:
    https://github.com/OmarAflak/Android-Camera2-Library

--- app/src/main/java/com/gmail/gigi/dan2011/ehealthsupermarket/ui/scan/scanFragment.java
+++ app/src/main/java/com/gmail/gigi/dan2011/ehealthsupermarket/ui/scan/scanFragment.java
@@ -1,26 +1,71 @@
 package com.gmail.gigi.dan2011.ehealthsupermarket.ui.scan;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.gmail.gigi.dan2011.ehealthsupermarket.R;
+import com.google.android.material.button.MaterialButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 public class scanFragment extends Fragment {
 
-    private scanViewModel scanViewModel;
+    private MaterialButton qrButton; // or Button type if not working
+    private MaterialButton smartButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        scanViewModel = new ViewModelProvider(this).get(scanViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scan, container, false);
+        qrButton = root.findViewById(R.id.qrButton);
+        smartButton = root.findViewById(R.id.smartButton);
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(scanFragment.this.getContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(scanFragment.this.getActivity(), new String[]{
+                            Manifest.permission.CAMERA
+                    }, 100);
+                }
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+        smartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(scanFragment.this.getContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(scanFragment.this.getActivity(), new String[]{
+                            Manifest.permission.CAMERA
+                    }, 100);
+                }
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); /* https://github.com/OmarAflak/Android-Camera2-Library */
+                startActivityForResult(intent, 100);
+            }
+        });
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data"); // here we get the image in bitmap format
+        }
     }
 }
