44cfeecc5fb2ae791d054af05fd4840223aa8565
Gigi Marcel Dan <gigi.dan2011@gmail.com>
2021-06-21 20:23:06 +0200

    Rearranged classes

---
+++ eHealthSupermarket/app/src/main/java/com/gmail/gigi/dan2011/ehealthsupermarket/sections/list/CustomAdapter.java
@@ -0,0 +1,67 @@
+package com.gmail.gigi.dan2011.ehealthsupermarket.sections.list;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.gmail.gigi.dan2011.ehealthsupermarket.R;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Javadoc comment.
+ */
+public class CustomAdapter extends BaseAdapter implements Serializable {
+
+  public List<RowItem> singleRow;
+  private LayoutInflater thisInflater;
+
+
+  /**
+   * Javadoc comment.
+   */
+  public CustomAdapter(Context context, List<RowItem> aaRow) {
+
+    this.singleRow = aaRow;
+    thisInflater = (LayoutInflater.from(context));
+
+  }
+
+  @Override
+  public int getCount() {
+    return singleRow.size();
+  }
+
+  @Override
+  public RowItem getItem(int position) {
+    return singleRow.get(position);
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return position;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    if (convertView == null) {
+      convertView = thisInflater.inflate(R.layout.fragment_mylist_row, parent, false);
+
+      TextView theHeading = (TextView) convertView.findViewById(R.id.textHeading);
+      TextView theSubHeading = (TextView) convertView.findViewById(R.id.textSubHeading);
+      ImageView theImage = (ImageView) convertView.findViewById(R.id.imageView_product);
+      RowItem currentRow = (RowItem) getItem(position);
+
+      theHeading.setTextColor(Color.BLACK);
+      theHeading.setText(currentRow.getListName());
+      theSubHeading.setText(currentRow.getSubHeading());
+      theImage.setImageResource(currentRow.getSmallImageName());
+    }
+    return convertView;
+  }
+
+}
---
+++ eHealthSupermarket/app/src/main/java/com/gmail/gigi/dan2011/ehealthsupermarket/sections/scan/ScanViewModel.java
@@ -0,0 +1,10 @@
+package com.gmail.gigi.dan2011.ehealthsupermarket.sections.scan;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * Javadoc comment.
+ */
+public class ScanViewModel extends ViewModel {
+
+}
